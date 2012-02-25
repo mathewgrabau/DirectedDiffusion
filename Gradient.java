63945ecd7a7c1e7cf488c7daf1f1e66bb9a951b2
Mathew Grabau <mgrabau@shaw.ca>
2012-02-24 20:10:45 -0600

    Started working on big improvements: - updated the gradient to add a ton of methods - Implements some common Java interfaces as well.

--- Gradient.java
+++ Gradient.java
@@ -1,6 +1,75 @@
-public class Gradient
+
+
+public class Gradient implements Comparable<Gradient>
 {
-  public Gradient()
+  private int sender; // ID of the sender
+  private int rate;
+
+  public Gradient(int sender, int rate)
   {
+    this.sender = sender;
+    this.rate = rate;
+  }
+
+  public int getSender()
+  {
+    return sender;
+  }
+
+  public int getRate()
+  {
+    return rate;
+  }
+
+  public boolean equals(Object other)
+  {
+    boolean isEqual = false;
+    Gradient g;
+
+    if (other != null && other instanceof Gradient)
+    {
+      g = (Gradient)other;
+      isEqual = (sender == g.sender) && (rate == g.rate);
+    }
+
+    return isEqual;
+  }
+
+  // required by the Comparable interface
+  public int compareTo(Gradient g)
+  {
+    if (equals(g))
+    {
+      return 0;
+    }
+    else
+    {
+      // perform the required 
+      if (sender < g.sender)
+      {
+        return -1;
+      }
+      else if (sender > g.sender)
+      {
+        return 1;
+      }
+      else
+      {
+        // only getting here when the senders are the same.
+        // check the rate
+        if (rate < g.rate)
+        {
+          return -1;
+        }
+        else if (rate > g.rate)
+        {
+          return 1;
+        }
+        else
+        {
+          return 0;
+        }
+      }
+    }
   }
 }
