cbbb6b2dff63347c70dff8252baca4208ddeb280
Mathew Grabau <mgrabau@shaw.ca>
2012-03-08 01:46:18 -0600

    Started building the basic UI panel. Got frame for NodeListPanel, NodeListFrame and NodeCountPanel laid out. NodeCountFrame contains some of the code needed.
    Also added the tweaks for eclipse to use proper project formatting.

---
+++ ui/NodeListFrame.java
@@ -0,0 +1,41 @@
+/**
+ * A basic frame that 
+ * 
+ * It depends on:
+ * NodeListPanel
+ * NodeCountPanel
+ */
+package ui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ * @author mgrabau
+ *
+ */
+public class NodeListFrame extends JFrame 
+{
+  private NodeListPanel listPanel;
+  private NodeCountPanel countPanel;
+  
+  private JButton closeButton;
+  
+  public NodeListFrame(String title) 
+  {
+    
+  }
+  
+  public void addNode()
+  {
+  
+  }
+  
+  
+  /**
+   * Returns the panel to it's initial state.
+   */
+  public void reset()
+  {
+  }
+}
