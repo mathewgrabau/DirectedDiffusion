66c24db1e286d41f0854143480a429af97a7f9b2
Mathew Grabau <mgrabau@shaw.ca>
2012-03-15 23:16:22 -0500

    Updated the documentation and removed a debugging statement.

--- dd/ui/PointRenderer.java
+++ dd/ui/PointRenderer.java
@@ -1,6 +1,7 @@
 /**
  * DirectedDiffusion
- * 
+ *  
+ *  Renders the java.awt.Point at (x, y) pairs.
  */
 package dd.ui;
 
@@ -11,7 +12,7 @@ import javax.swing.table.DefaultTableCellRenderer;
  * 
  * Attach using JTable.setDefaultRenderer
  * 
- * @author Mathew Grabau
+ * @author Mathew Grabau (mgrabau)
  *
  */
 public class PointRenderer extends DefaultTableCellRenderer 
@@ -21,15 +22,29 @@ public class PointRenderer extends DefaultTableCellRenderer
     super();
   }
   
+  /***
+   * Sets the cell text for points.
+   * 
+   * @param value The value to convert (must be type java.awt.Point)
+   * @throws IllegalArgumentException if the value is not an instance of java.awt.Point
+   * 
+   * Just returns a string with "null" if the argument is null
+   */
   public void setValue(Object value) 
   {
-    
     String v = "";
-    System.out.println("setValue is called");
-    if (value != null && value instanceof java.awt.Point)
+    if (value == null)
+    {
+      v = "null";
+    }
+    else if(value instanceof java.awt.Point)
     {
       java.awt.Point p = (java.awt.Point)value;
-      v = String.format("(%d, %d)", p.x, p.y);
+      v = String.format("(%d, %d)", p.x, p.y);    
+    }
+    else 
+    {
+      throw new IllegalArgumentException("Cannot render type " + value.getClass().getName());
     }
     
     setText(v);
