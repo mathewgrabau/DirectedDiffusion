6f52cd71f62a056efca582337df0a3309f956be8
Mathew Grabau <mgrabau@shaw.ca>
2012-03-17 22:04:33 -0500

    Added the enumeration definition for the logging levels.

---
+++ dd/ui/log/LogLevel.java
@@ -0,0 +1,19 @@
+/**
+ * DirectedDiffusion
+ */
+package dd.ui.log;
+
+/**
+ * Defines the allowable event log levels.
+ * 
+ * @author Mathew Grabau
+ *
+ */
+public enum LogLevel
+{
+  NONE,
+  DEBUG,
+  INFO,
+  WARN,
+  ERROR
+}
