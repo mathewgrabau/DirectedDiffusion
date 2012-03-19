3ba0101646c50a7328d5f36b87dc9441df15e25c
Mathew Grabau <mgrabau@shaw.ca>
2012-03-19 00:01:55 -0500

    Refined the test to put in a timer that generates some events for testing things out.

--- dd/ui/test/LogTest.java
+++ dd/ui/test/LogTest.java
@@ -3,11 +3,17 @@
  */
 package dd.ui.test;
 
+import java.util.Date;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 import dd.ui.log.LogFrame;
+import dd.ui.log.LogLevel;
 
 /**
  * @author Mathew Grabau
@@ -17,11 +23,36 @@ public class LogTest
 {
   static void createAndShowGUI()
   {
-    LogFrame lf = new LogFrame("LogFrame Demo");
-    lf.pack();
+    final Random random = new Random();
+    
+    final LogFrame lf = new LogFrame("LogFrame Demo");
     lf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    lf.pack();
     lf.setVisible(true);
     
+    // 1) Set the timer to add an event every so often to the log
+    Timer t = new Timer("loggingTimer");
+    
+    lf.addMessage(LogLevel.INFO, "Created the timer");
+    
+    // schedule the task to delay two seconds, and pulse every second
+    // each event is issued with random taskings.
+    // made it fairly slow due to the need to keep it workable.
+    t.schedule(new TimerTask() {
+
+      @Override
+      public void run()
+      {
+        // generate an event
+        int ordinal = random.nextInt(4);
+        // don't want to get the NONE being posted
+        LogLevel l = LogLevel.values()[ordinal + 1];
+        
+        lf.addMessage(l, "a timed test event was fired");
+      }
+      
+    }, 2000, 2000);
+
   }
 
   /**
@@ -38,9 +69,7 @@ public class LogTest
     SwingUtilities.invokeLater(new Runnable() {
       
       public void run()
-      {
-        
-        
+      {        
         createAndShowGUI();
       }
     });
